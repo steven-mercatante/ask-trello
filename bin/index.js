9fd154a287ff2f44bb74fb27d1181e092ccbe3ff
Steven Mercatante <steven.mercatante@gmail.com>
2019-09-23 17:25:15 -0400

    use commandDir

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const ideaCmd = require("./commands/idea");
-const configCmd = require("./commands/config");
 
-/**
- * ask config topic // shows list of topics and prompts user to select one
- * ask config --unset topic
- * ask config info // outputs path to config file & existing config values
- * ask config board [value] // prints existing boardId value, or sets new one
- */
-
-// TODO: you should be able to call config command without having been prev authenticated
-
-yargs.command(ideaCmd).command(configCmd).argv;
+yargs.commandDir("commands").help().argv;
